0de9df15a116250f841b29941b4651111c2c653f
Adam Feuer <adam@starcat.io>
2023-03-30 20:19:29 -0700

    WIP - can blink SAMA5D2-XULT RGB LED - blue blink

--- boards/arm/sama5/sama5d2-xult/src/sam_appinit.c
+++ boards/arm/sama5/sama5d2-xult/src/sam_appinit.c
@@ -26,10 +26,11 @@
 #include <syslog.h>
 #include <debug.h>
 
-#include <nuttx/board.h>
+#include <sam_pio.h>
 
 #include "sama5d2-xult.h"
 
+#include <nuttx/board.h>
 #ifndef CONFIG_BUILD_KERNEL
 
 /****************************************************************************
@@ -41,6 +42,8 @@
  ****************************************************************************/
 
 /*
+*/
+
 #define LOW false
 #define HIGH true
 
@@ -52,43 +55,82 @@
 #undef IPIN_DRDY
 #undef PIN_CS
 
-#define PIN_LED         (GPIO_PORT_PIOB | GPIO_PIN27 | GPIO_OUTPUT)
-#define IPIN_PWDN       (GPIO_PORT_PIOC | GPIO_PIN1 | GPIO_OUTPUT)
-#define PIN_CLKSEL      (GPIO_PORT_PIOC | GPIO_PIN15 | GPIO_OUTPUT)
-#define IPIN_RESET      (GPIO_PORT_PIOC | GPIO_PIN16 | GPIO_OUTPUT)
-#define PIN_START       (GPIO_PORT_PIOA | GPIO_PIN4 | GPIO_OUTPUT)
-#define IPIN_DRDY       (GPIO_PORT_PIOA | GPIO_PIN15 | GPIO_OUTPUT)
-#define PIN_CS          (GPIO_PORT_PIOA | GPIO_PIN14 | GPIO_OUTPUT)
+
+// Arduino Due pin: D13
+// SAMA5 pin: Blue: PB0
+// SAMA5 pin: Green: PB5
+// const int PIN_LED = 13;
+#define PIN_LED_GREEN   (PIO_CFG_DEFAULT | PIO_PORT_PIOB | PIO_PIN5 | PIO_OUTPUT)
+#define PIN_LED_BLUE    (PIO_CFG_DEFAULT | PIO_PORT_PIOB | PIO_PIN0 | PIO_OUTPUT)
+
+// Arduino Due pin: D33 
+// SAMA5 pin: PB28 
+// const int IPIN_PWDN = 33;
+#define IPIN_PWDN       (PIO_CFG_DEFAULT | PIO_PORT_PIOB | PIO_PIN28 | PIO_OUTPUT)
+// Arduino Due pin: D48 
+// SAMA5 pin: PC7 
+// const int PIN_CLKSEL = 48;
+#define PIN_CLKSEL     (PIO_CFG_DEFAULT | PIO_PORT_PIOC | PIO_PIN7 | PIO_OUTPUT) 
+// Arduino Due pin: D47
+// SAMA5 pin: PB12
+// const int IPIN_RESET = 47;
+#define IPIN_RESET     (PIO_CFG_DEFAULT | PIO_PORT_PIOB | PIO_PIN12 | PIO_OUTPUT) 
+
+// Arduino Due pin: A5
+// SAMA5 pin: PD22
+// const int PIN_START = 59;
+#define PIN_START      (PIO_CFG_DEFAULT | PIO_PORT_PIOD | PIO_PIN22 | PIO_OUTPUT) 
+// Arduino Due pin: D24 
+// SAMA5 pin: PA24 
+// const int IPIN_DRDY = 24;   // board 0: JP1, pos. 1
+#define IPIN_DRDY      (PIO_CFG_DEFAULT | PIO_PORT_PIOA | PIO_PIN24 | PIO_OUTPUT) 
+//const int IPIN_DRDY = 25; // board 1: JP1, pos. 2
+//const int IPIN_DRDY = 26; // board 2: JP1, pos. 3
+//const int IPIN_DRDY = 27; // board 3: JP1, pos. 4
+
+// Arduino Due pin: D23
+// SAMA5 pin: PA14 
+// const int PIN_CS = 23;   // board 0: JP2, pos. 3
+#define PIN_CS         (PIO_CFG_DEFAULT | PIO_PORT_PIOA | PIO_PIN14 | PIO_CFGR_DIR_OUTPUT) 
 
 void test_gpio(void) {
   spiinfo("test_gpio: starting.");
   spiinfo("PIN_START configuration: (hex): %x\n", PIN_START);
   spiinfo("PIN_CLKSEL configuration: (hex): %x\n", PIN_CLKSEL);
   spiinfo("PIN_CS configuration: (hex): %x\n", PIN_CS);
-  sam_configgpio(PIN_LED);
-  sam_configgpio(PIN_START);
-  sam_configgpio(IPIN_PWDN);
-  sam_configgpio(PIN_CLKSEL);
-  sam_configgpio(IPIN_RESET);
-  sam_configgpio(PIN_CS);
-  while (true) {
-    sam_gpiowrite(PIN_LED, LOW);
-    sam_gpiowrite(PIN_START, LOW);
-    sam_gpiowrite(IPIN_PWDN, LOW);
-    sam_gpiowrite(PIN_CLKSEL, LOW);
-    sam_gpiowrite(IPIN_RESET, LOW);
-    sam_gpiowrite(PIN_CS, LOW);
+  sam_configpio(PIN_LED_GREEN);
+  sam_configpio(PIN_LED_BLUE);
+  sam_configpio(PIN_START);
+  sam_configpio(IPIN_PWDN);
+  sam_configpio(PIN_CLKSEL);
+  sam_configpio(IPIN_RESET);
+  sam_configpio(PIN_CS);
+  sam_piowrite(PIN_LED_BLUE, HIGH); // Blue LED off
+  sam_piowrite(PIN_LED_GREEN, HIGH); // Green LED off
+  sam_piowrite(PIN_LED_BLUE, LOW); // Blue LED on
+  for (int i=0; i<50; i++) {
+    sam_piowrite(PIN_LED_BLUE, LOW);
     usleep(200*1000);
-    sam_gpiowrite(PIN_LED, HIGH);
-    sam_gpiowrite(PIN_START, HIGH);
-    sam_gpiowrite(IPIN_PWDN, HIGH);
-    sam_gpiowrite(PIN_CLKSEL, HIGH);
-    sam_gpiowrite(IPIN_RESET, HIGH);
-    sam_gpiowrite(PIN_CS, HIGH);
+    sam_piowrite(PIN_LED_BLUE, HIGH);
     usleep(200*1000);
   }
+  // while (true) {
+  //   sam_piowrite(PIN_LED, LOW);
+  //   sam_piowrite(PIN_START, LOW);
+  //   sam_piowrite(IPIN_PWDN, LOW);
+  //   sam_piowrite(PIN_CLKSEL, LOW);
+  //   sam_piowrite(IPIN_RESET, LOW);
+  //   sam_piowrite(PIN_CS, LOW);
+  //   usleep(200*1000);
+  //   sam_piowrite(PIN_LED, HIGH);
+  //   sam_piowrite(PIN_START, HIGH);
+  //   sam_piowrite(IPIN_PWDN, HIGH);
+  //   sam_piowrite(PIN_CLKSEL, HIGH);
+  //   sam_piowrite(IPIN_RESET, HIGH);
+  //   sam_piowrite(PIN_CS, HIGH);
+  //   usleep(200*1000);
+  // }
 }
-*/
 
 /****************************************************************************
  * Name: board_ioctl
@@ -176,7 +218,10 @@ int board_app_initialize(uintptr_t arg)
   /* Perform board initialization */
 
   mcinfo("Entry\n");
-  return sam_bringup();
+  // return sam_bringup();
+  int ret = sam_bringup();
+  test_gpio();
+  return ret;
 #else
   return OK;
 #endif
